242a3bbe130db0d0c2802a7fab57923d9b9b58a5
rafael <rafaelmatosqa@gmail.com>
2022-08-25 23:39:43 -0400

    ajustes na classe run para pipeline

--- src/test/java/integracao/rest/RestApplicationTests.java
+++ src/test/java/integracao/rest/RestApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RestApplicationTests {
+ class RestApplicationTests {
 
 	@Test
-	void contextLoads() {
+	 void contextLoads() {
 	}
 
 }
